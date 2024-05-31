727fe66c33b994f96d2425dee7467030c23e74a6
David Ayllón Martín <davidayllonmartin@protonmail.com>
2024-05-31 14:43:50 +0200

    Commit

---
+++ src/main/java/org/ielena/pokedex/singletons/SpringContextSingleton.java
@@ -0,0 +1,16 @@
+package org.ielena.pokedex.singletons;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class SpringContextSingleton {
+
+    @Setter
+    @Getter
+    private static ConfigurableApplicationContext context;
+
+    private SpringContextSingleton() {}
+
+}
+
